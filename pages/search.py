2a24189f1104003cda2bc8bee967a9243efaf109
Joshua Mutiso <67162405+Mozzie455@users.noreply.github.com>
2024-05-08 13:22:02 +0000

    configuring multiple browsers

--- pages/search.py
+++ pages/search.py
@@ -6,23 +6,27 @@ the page object for the DuckDuckGo search page.
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 class DuckDuckGoSearchPage:
 
-    # URL
-    URL = 'https://www.duckduckgo.com'
+  # URL
 
-    # Locators 
-    SEARCH_INPUT = (By.ID, 'search_from_input_homepage')
+  URL = 'https://www.duckduckgo.com'
 
-    # Initializer
-    def __init__(self, browser):
-        self.browser = browser
+  # Locators
 
-    # Interaction Methods
-    def load(self):
-        self.browser.get(self.URL)
+  SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
 
-    def search(self, pharse):
-        search_input = self.browser.find_element(*self.SEARCH_INPUT)
-        search_input.send_keys(phrase + Keys.RETURN)
-        
+  # Initializer
+
+  def __init__(self, browser):
+    self.browser = browser
+
+  # Interaction Methods
+
+  def load(self):
+    self.browser.get(self.URL)
+
+  def search(self, phrase):
+    search_input = self.browser.find_element(*self.SEARCH_INPUT)
+    search_input.send_keys(phrase + Keys.RETURN)
