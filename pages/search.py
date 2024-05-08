1c6c7375279efa1913961f7931177fdc04be850d
Joshua Mutiso <67162405+Mozzie455@users.noreply.github.com>
2024-05-08 09:11:15 +0000

    defining page elements

---
+++ pages/search.py
@@ -0,0 +1,19 @@
+"""
+This module contains DuckDuckGoSearchPage,
+the page object for the DuckDuckGo search page.
+"""
+
+
+class DuckDuckGoSearchPage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def load(self):
+        # TODO
+        pass
+
+    def search(self, pharse):
+        # TODO
+        pass
+        
